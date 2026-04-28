7569bda67d864f406f072cb2697cca842129137e
Ingrid <ingridbbee@gmail.com>
2026-04-28 16:51:37 -0700

    kd tree code

---
+++ src/AxisComparator.java
@@ -0,0 +1,22 @@
+import java.util.Comparator;
+
+public class pointComparator implements Comparator<UberDriver> {
+    private final int axis; // 0 = x, 1 = y
+
+    public pointComparator(int axis) {
+        this.axis = axis;
+    }
+
+    @Override
+    public int compare(UberDriver a, UberDriver b) {
+        double a1 = (axis == 0) ? a.getX() : a.getY();
+        double b1 = (axis == 0) ? b.getX() : b.getY();
+        int c = Double.compare(a1,b1);
+        if(c != 0) return c;
+        // tie breaker if c == 0, then compare y
+        double a2 = (axis == 0) ? a.getY() : a.getX();
+        double b2 = (axis == 0) ? b.getY() : b.getX();
+        return Double.compare(a2,b2);
+
+    }
+}
